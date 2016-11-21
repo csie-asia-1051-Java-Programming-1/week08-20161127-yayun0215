6ff81ee25edb25fb0a28535cf250840c96b66377
Yi-Cheng Chen <Chad.CYB@gmail.com>
2016-11-21 14:10:43 +0800

    week8 ex change

--- src/ex/ex04.java
+++ src/ex/ex04.java
@@ -1,11 +1,11 @@
 ﻿package ex;
 /*
- * Topic: 請寫一個函式 (不是遞迴) 進行十進制數字轉換成二進制數字，例如十進制的 13 會輸出二進制的 1101
+ * Topic: 請用類別撰寫一個計算機，透過計算機物件可以計算 加、減、乘、除、平方、立方、開根號的功能
  * Date: 2016/11/21
  * Author: 1050210XX 周永振老師
  */
 
-public class ex01 {
+public class ex04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
